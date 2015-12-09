845a3b6dcd1d760781145c6a316d78bd5e824831
Nicolai Østergaard <nicolai76@live.dk>
2015-12-09 17:20:38 +0100

    First commit to GitHub due to lack of knowledge about server repo changes: Highscore class has been created. hardcoded db connection code in databasedriver changed back to the original one which takes the variables from the config.json file.

---
+++ src/model/Highscore.java
@@ -0,0 +1,58 @@
+package model;
+
+/**
+ * Created by nicolaiostergaard on 04/12/15.
+ */
+public class Highscore {
+
+    int no1;
+    int no2;
+    int no3;
+    int no4;
+    int no5;
+
+    public Highscore(){
+
+
+    }
+
+    public int getNo1() {
+        return no1;
+    }
+
+    public void setNo1(int no1) {
+        this.no1 = no1;
+    }
+
+    public int getNo2() {
+        return no2;
+    }
+
+    public void setNo2(int no2) {
+        this.no2 = no2;
+    }
+
+    public int getNo3() {
+        return no3;
+    }
+
+    public void setNo3(int no3) {
+        this.no3 = no3;
+    }
+
+    public int getNo4() {
+        return no4;
+    }
+
+    public void setNo4(int no4) {
+        this.no4 = no4;
+    }
+
+    public int getNo5() {
+        return no5;
+    }
+
+    public void setNo5(int no5) {
+        this.no5 = no5;
+    }
+}
---
+++ src/model/Scores.java
@@ -0,0 +1,49 @@
+package model;
+
+/**
+ * Created by nicolaiostergaard on 04/12/15.
+ */
+public class Scores {
+
+
+    int score;
+    int userId;
+    int opponentId;
+    int gameId;
+
+    public Scores() {
+
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getOpponentId() {
+        return opponentId;
+    }
+
+    public void setOpponentId(int opponentId) {
+        this.opponentId = opponentId;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+}
